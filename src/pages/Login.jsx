ed61e7600f8752e330592aba58576fd95f9978d1
bekirbali <bfbali43@gmail.com>
2023-06-27 16:47:26 +0300

    home login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,26 @@ import styles from "../styles/login.module.scss";
 const Login = () => {
   return (
     <div className={styles.login}>
-      <h1>Login</h1>
+      <div className={styles.card}>
+        <h1>manage courses</h1>
+        <h2>sign in</h2>
+        <p>Enter your credentials to access your account</p>
+        <form className={styles.form}>
+          <div className="emails">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="Enter your email" />
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button type="submit">sign in</button>
+        </form>
+      </div>
     </div>
   );
 };
