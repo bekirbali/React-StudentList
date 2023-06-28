c11b16b9f166bdff64303176aa6ef9bb05989abf
bekirbali <bfbali43@gmail.com>
2023-06-28 22:45:40 +0300

    private router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import styles from "../styles/home.module.scss";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>home</h1>
+    <div className={styles.home}>
+      <nav className={styles.navbar}>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li onClick={() => navigate("students")}>
+            {/* <Link to="/students">Students</Link> */}
+            students
+          </li>
+          {/* <li>
+            <Link to="/students">Course</Link>
+          </li> */}
+          {/* <li><Link to=''>Payment</Link></li> */}
+          {/* <li><Link to=''>Report</Link></li> */}
+          {/* <li><Link to=''>Settings</Link></li> */}
+        </ul>
+      </nav>
+      <div className={styles.outlet}>
+        <Outlet />
+      </div>
     </div>
   );
 };
