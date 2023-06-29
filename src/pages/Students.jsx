50975c22dd5a713c909a47d549f0f2bb7b46af5f
bekirbali <bfbali43@gmail.com>
2023-06-29 18:09:56 +0300

    outlet

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Students = () => {
-  return <div>Students</div>;
+  return (
+    <div>
+      <h1>Students</h1>
+      <ul>
+        <li>asdgsdgkhl</li>
+        <li>asdgsdgkhl</li>
+        <li>asdgsdgkhl</li>
+        <li>asdgsdgkhl</li>
+        <li>asdgsdgkhl</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Students;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,14 +4,17 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import PrivateRouter from "./PrivateRouter";
 import Students from "../pages/Students";
+import Dashboard from "../pages/Dashboard";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" element={<PrivateRouter />}>
-        <Route index element={<Home />} />
-        <Route path="students" element={<Students />} />
+      <Route path="dashboard" element={<PrivateRouter />}>
+        <Route path="" element={<Dashboard />}>
+          <Route index element={<Home />} />
+          <Route path="students" element={<Students />} />
+        </Route>
       </Route>
     </Routes>
   );
