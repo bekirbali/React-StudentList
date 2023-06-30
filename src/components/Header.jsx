5497e28fdf8a8691aa8c95f6526df0b6d088cabf
bekirbali <bfbali43@gmail.com>
2023-06-30 23:31:13 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "../styles/header.module.scss";
+
+import { TbCircleCaretLeft } from "react-icons/tb";
+import { AiOutlineBell } from "react-icons/ai";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.header}>
+      <TbCircleCaretLeft size={20} />
+      <AiOutlineBell size={20} />
+    </div>
+  );
 };
 
 export default Header;
