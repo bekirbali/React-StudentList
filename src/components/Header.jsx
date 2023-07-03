7ab72a7bc0978a9455eb627519f890a327aa0d86
bekirbali <bfbali43@gmail.com>
2023-07-03 23:27:20 +0300

    payment report header settings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/header.module.scss";
 
 import { TbCircleCaretLeft } from "react-icons/tb";
 import { AiOutlineBell } from "react-icons/ai";
+import { BsBell, BsBellSlash } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [bell, setBell] = useState(true);
   const navigate = useNavigate();
   return (
     <div className={styles.header}>
@@ -14,7 +16,19 @@ const Header = () => {
         onClick={() => navigate(-1)}
         style={{ cursor: "pointer" }}
       />
-      <AiOutlineBell size={20} style={{ cursor: "pointer" }} />
+      {bell ? (
+        <BsBell
+          size={20}
+          style={{ cursor: "pointer" }}
+          onClick={() => setBell(!bell)}
+        />
+      ) : (
+        <BsBellSlash
+          size={20}
+          style={{ cursor: "pointer" }}
+          onClick={() => setBell(!bell)}
+        />
+      )}
     </div>
   );
 };
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "../styles/cards.module.scss";
+import { GiSettingsKnobs } from "react-icons/gi";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className={styles.home}>
+      <div className={`${styles.card} ${styles.settings}`}>
+        <div>
+          <GiSettingsKnobs size={60} style={{ border: "1px solid white" }} />
+          <h2>Report</h2>
+        </div>
+        <div className={styles.number}>
+          <h3>26</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
